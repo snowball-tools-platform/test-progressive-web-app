944bd4024d45e3f04a263f45e32aaf15014759ad
Thomas E Lackey <telackey@bozemanpass.com>
2023-12-07 23:01:30 +0000

    Register CRN on release (#6)
    Add a CRN for the application on release as well as a record.
    
    Reviewed-on: https://git.vdb.to/cerc-io/test-progressive-web-app/pulls/6
    Co-authored-by: Thomas E Lackey <telackey@bozemanpass.com>
    Co-committed-by: Thomas E Lackey <telackey@bozemanpass.com>

--- scripts/publish-app-record.sh
+++ scripts/publish-app-record.sh
@@ -6,7 +6,8 @@ RECORD_FILE=tmp.rf.$$
 CONFIG_FILE=`mktemp`
 
 CERC_APP_TYPE=${CERC_APP_TYPE:-"webapp"}
-CERC_REPO_TAG=${CERC_REPO_TAG:-${GITHUB_SHA:-main}}
+CERC_REPO_REF=${CERC_REPO_REF:-${GITHUB_SHA:-`git log -1 --format="%H"`}}
+CERC_IS_LATEST_RELEASE=${CERC_IS_LATEST_RELEASE:-"true"}
 
 rcd_name=$(jq -r '.name' package.json | sed 's/null//')
 rcd_desc=$(jq -r '.description' package.json | sed 's/null//')
@@ -19,14 +20,14 @@ rcd_app_version=$(jq -r '.version' package.json | sed 's/null//')
 cat <<EOF > "$CONFIG_FILE"
 services:
   cns:
-    restEndpoint: '${CERC_LACONIC_REST_ENDPOINT:-http://console.laconic.com:1317}'
-    gqlEndpoint: '${CERC_LACONIC_GQL_ENDPOINT:-http://console.laconic.com:9473/api}'
-    chainId: ${CERC_LACONIC_CHAIN_ID:-laconic_9000-1}
-    gas: 350000
+    restEndpoint: '${CERC_REGISTRY_REST_ENDPOINT:-http://console.laconic.com:1317}'
+    gqlEndpoint: '${CERC_REGISTRY_GQL_ENDPOINT:-http://console.laconic.com:9473/api}'
+    chainId: ${CERC_REGISTRY_CHAIN_ID:-laconic_9000-1}
+    gas: 550000
     fees: 200000aphoton
 EOF
 
-next_ver=$(laconic -c $CONFIG_FILE cns record list --type ApplicationRecord --all --name "$rcd_name" 2>/dev/null | jq -r -s ".[] | sort_by(.createTime) | reverse | [ .[] | select(.bondId == \"$CERC_LACONIC_BOND_ID\") ] | .[0].attributes.version" | awk -F. -v OFS=. '{$NF += 1 ; print}')
+next_ver=$(laconic -c $CONFIG_FILE cns record list --type ApplicationRecord --all --name "$rcd_name" 2>/dev/null | jq -r -s ".[] | sort_by(.createTime) | reverse | [ .[] | select(.bondId == \"$CERC_REGISTRY_BOND_ID\") ] | .[0].attributes.version" | awk -F. -v OFS=. '{$NF += 1 ; print}')
 
 if [ -z "$next_ver" ] || [ "1" == "$next_ver" ]; then
   next_ver=0.0.1
@@ -42,13 +43,23 @@ record:
   license: "$rcd_license"
   author: "$rcd_author"
   repository: "$rcd_repository"
-  repository_tag: "$CERC_REPO_TAG"
+  repository_ref: "$CERC_REPO_REF"
   app_version: "$rcd_app_version"
   app_type: "$CERC_APP_TYPE"
 EOF
 
 
 cat $RECORD_FILE
-laconic -c $CONFIG_FILE cns record publish --filename $RECORD_FILE --user-key "${CERC_LACONIC_USER_KEY}" --bond-id ${CERC_LACONIC_BOND_ID}
+RECORD_ID=$(laconic -c $CONFIG_FILE cns record publish --filename $RECORD_FILE --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} | jq -r '.id')
+echo $RECORD_ID
+
+if [ -n "$CERC_REGISTRY_APP_CRN" ]; then
+  laconic -c $CONFIG_FILE cns name set --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} "$CERC_REGISTRY_APP_CRN@${rcd_app_version}" "$RECORD_ID"
+  laconic -c $CONFIG_FILE cns name set --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} "$CERC_REGISTRY_APP_CRN@${CERC_REPO_REF}" "$RECORD_ID"
+  if [ "true" == "$CERC_IS_LATEST_RELEASE" ]; then
+    laconic -c $CONFIG_FILE cns name set --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} "$CERC_REGISTRY_APP_CRN" "$RECORD_ID"
+    laconic -c $CONFIG_FILE cns name set --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} "$CERC_REGISTRY_APP_CRN@latest" "$RECORD_ID"
+  fi
+fi
 
 rm -f $RECORD_FILE $CONFIG_FILE
